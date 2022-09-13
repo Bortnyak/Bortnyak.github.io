fc5b77a480754adceadfd93dcdf63c08ca061524
Volodymyr Bortniak <volodymyr@Volodymyrs-MacBook-Pro.local>
2022-09-14 00:22:53 +0300

    path changed

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 
 function startTimer() {
   console.log("Hey, I'm here...");
-  const worker = new Worker("timer.js");
+  const worker = new Worker("src/timer.js");
   const display = document.querySelector('#time');
   const duration = 5000;
   const workerMessage = { duration, display };
