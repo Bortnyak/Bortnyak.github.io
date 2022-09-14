28f400e5c6fcd1daeaab449a1975c20fa45eb475
Volodymyr Bortniak <volodymyr@Volodymyrs-MacBook-Pro.local>
2022-09-14 10:13:25 +0300

    timer --> worker

--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 function startTimer() {
   console.log("Hey, I'm here...");
   
-  const worker = new Worker("src/timer.js");
+  const worker = new Worker("src/worker.js");
   const display = document.querySelector('#time');
   const duration = 5000;
   const workerMessage = { duration, display };
